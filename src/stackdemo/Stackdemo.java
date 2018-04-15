9082235cde9ce6b1043e4e1864ed53b756d29b6c
unknown <pipaliyaatul@gmail.com>
2018-04-15 10:53:54 -0400

    indentation changes

--- src/stackdemo/Stackdemo.java
+++ src/stackdemo/Stackdemo.java
@@ -1,4 +1,3 @@
-
 package stackdemo;
 import java.util.*;
 
@@ -11,55 +10,56 @@ import java.util.*;
         stkdemo.push(10);
         stkdemo.push(20);
         stkdemo.push(30);
+        stkdemo.push(45);
         stkdemo.pop();
      
         stkdemo.size();
     }
-         
-     int top=-1;
-     int size=0;
-     int[] stk=new int[2];
+
+    int top=-1;
+    int size=0;
+    int[] stk=new int[2];
      
-     void push(int x)
-     {
-          if(size==stk.length)
-         {
-             
-         System.out.println("Can not insert");
-      
-         
-         }          
-         else
-         {
-            stk[size]=x;
-            size++;
-         }
-      
-         
-         System.out.println("Stack now is"+x);
-         
-     }
-     void pop()
-     {
-           if(size==0 )               
-           {
-               System.out.println("Stack is underflow cant delete any element");
-           }
-           else 
-           {
-               int result=stk[size-1];
-           size--;              
-           System.out.println("Stack returned value is"+result);
-           }
-      }
-     void size()
-     {
-         int size=stk.length;
-         
-         System.out.println("The stack length is"+size);
-     }
+    void push(int x)
+    {
+        if(size==stk.length)
+        {
+            System.out.println("Can not insert");
+        }          
+        else
+        {
+           stk[size]=x;
+           size++;
+        }
+
+
+        System.out.println("Stack now is"+x);
+
     }
-  
+     
+    void pop()
+    {
+
+        if(size==0 )               
+        {
+            System.out.println("Stack is underflow cant delete any element");
+        }
+        else 
+        {
+            int result=stk[size-1];
+        size--;              
+        System.out.println("Stack returned value is"+result);
+        }
+    }
+    
+    void size()
+    {
+        int size=stk.length;
+
+        System.out.println("The stack length is"+size);
+    }
+ }
+
     
 
 
